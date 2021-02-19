b4448736cb36eed1b7d8e3d631c3d71f3c38536d
JuanMa Velez <juanmanuelveleztorres@gmail.com>
2021-02-19 01:01:22 +0100

    removing .env, adding those variables to the enviroments folder

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
   API_URL: `http://localhost:3000/api`,
+  PORT: 8000,
+  API_KEY_TOKEN:
+    'fc8c70233ac378225f392e349b20be14626d958d7d1aad9444db90c854916607',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,9 @@
 export const environment = {
   production: false,
   API_URL: `http://localhost:3000/api`,
+  PORT: 8000,
+  API_KEY_TOKEN:
+    'fc8c70233ac378225f392e349b20be14626d958d7d1aad9444db90c854916607',
 };
 
 /*
