0595e51b16309dca490e8cade82d2fbd7f88037e
JuanMa Velez <juanmanuelveleztorres@gmail.com>
2021-02-24 15:13:18 +0100

    Installing, @types/passport, @types/cookieParser, adding models of the auth code

--- server.ts
+++ server.ts
@@ -9,8 +9,16 @@ import { APP_BASE_HREF } from '@angular/common';
 import { existsSync } from 'fs';
 import { environment } from './src/environments/environment';
 
-import { passport } from 'passport';
+import { authenticate } from 'passport';
 
+import { unauthorized, badImplementation } from '@hapi/boom';
+import * as cookieParser from 'cookie-parser';
+import axios from 'axios';
+import { IAuthRequest } from './server/models/AuthRequest.model';
+const THIRTY_DAYS_IN_SEC = 2592000;
+const TWO_HOURS_IN_SEC = 7200;
+
+require('./server/utils/strategies/basic');
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express();
@@ -39,6 +47,112 @@ export function app(): express.Express {
       maxAge: '1y',
     })
   );
+  server.use(express.json());
+  server.use(cookieParser());
+  server.post(
+    '/auth/sign-in',
+    async (
+      req: express.Request,
+      res: express.Response,
+      next: express.NextFunction
+    ) => {
+      const { remenberMe } = req.body;
+      authenticate('basic', (err: Error, data: IAuthRequest) => {
+        try {
+          if (err || !data) {
+            next(unauthorized());
+          }
+          req.login(data, { session: false }, async (err: Error) => {
+            if (err) {
+              next(err);
+            }
+            const { token, ...user } = data;
+
+            res.cookie('token', token, {
+              httpOnly: environment.production,
+              secure: environment.production,
+              maxAge: remenberMe ? THIRTY_DAYS_IN_SEC : TWO_HOURS_IN_SEC,
+            });
+            res.status(200).json(user);
+          });
+        } catch (err) {
+          next(err);
+        }
+      })(req, res, next);
+    }
+  );
+  server.post(
+    '/auth/sign-up',
+    async (
+      req: express.Request,
+      res: express.Response,
+      next: express.NextFunction
+    ) => {
+      const { body: user } = req;
+      try {
+        await axios({
+          url: `${environment.API_URL}/auth/sign-up`,
+          method: 'post',
+          data: user,
+        });
+
+        res.status(201).json({ message: 'user created' });
+      } catch (err) {
+        next(err);
+      }
+    }
+  );
+
+  server.post(
+    '/user-courses',
+    async (
+      req: express.Request,
+      res: express.Response,
+      next: express.NextFunction
+    ) => {
+      try {
+        const { body: userCourses } = req;
+        const { token } = req.cookies;
+        const { data, status } = await axios({
+          url: `${environment.API_URL}/user-courses`,
+          headers: { Authorization: `Bearer ${token}` },
+          method: 'post',
+          data: userCourses,
+        });
+        if (status !== 201) {
+          return next(badImplementation());
+        }
+        res.status(201).json(data);
+      } catch (err) {
+        next(err);
+      }
+    }
+  );
+  server.delete(
+    '/user-courses/:userCoursesId',
+    async (
+      req: express.Request,
+      res: express.Response,
+      next: express.NextFunction
+    ) => {
+      try {
+        const { userCoursesId } = req.params;
+        const { token } = req.cookies;
+        const { data, status } = await axios({
+          url: `${environment.API_URL}/user-courses/${userCoursesId}`,
+          headers: { Authorization: `Bearer ${token}` },
+          method: 'delete',
+          data: userCoursesId,
+        });
+        if (status !== 200) {
+          return next(badImplementation());
+        }
+        res.status(200).json(data);
+      } catch (err) {
+        next(err);
+      }
+    }
+  );
 
   // All regular routes use the Universal engine
   server.get('*', (req, res) => {
@@ -53,18 +167,8 @@ export function app(): express.Express {
 
 function run(): void {
   const port = environment.PORT || 4000;
-
   // Start up the Node server
   const server = app();
-
-  // Routes
-  server.use(express.json());
-  server.post('/auth/sign-in', async (res, req, next) => {});
-  server.post('/auth/sign-up', async (res, req, next) => {});
-  server.get('/users', async (res, req, next) => {});
-  server.post('/user-courses', async (res, req, next) => {});
-  server.delete('/user-courses/:userCoursesId', async (res, req, next) => {});
-
   server.listen(port, () => {
     console.log(`Node Express server listening on http://localhost:${port}`);
   });
---
+++ server/models/AuthRequest.model.ts
@@ -0,0 +1,4 @@
+export interface IAuthRequest {
+  token: string;
+  user: object;
+}
