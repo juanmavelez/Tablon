c41e27151a30990e81be5945401de5614443907a
JuanMa Velez <juanmanuelveleztorres@gmail.com>
2021-03-07 00:06:10 +0100

    userCourses, now workind. pipe async now it is working

--- src/app/courses/components/user-courses/user-courses.component.ts
+++ src/app/courses/components/user-courses/user-courses.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { UserCoursesService } from '@core/services/user-courses/user-courses.service';
-import { IResponseCourse } from '@core/models/course.model';
+import { ICourse, IResponseCourse } from '@core/models/course.model';
 
 @Component({
   selector: 'app-user-courses',
@@ -21,11 +21,9 @@ export class UserCoursesComponent implements OnInit {
   ngOnInit(): void {
     this.userCourses$ = this.activatedRoute.params.pipe(
       switchMap((params: Params) => {
-        console.log(params.id);
         return this.userCoursesService.getUserCourses(params.id);
       })
     );
+    this.userCourses$.subscribe((data) => console.log(data));
   }
-
-  fetchUserCourses(): void {}
 }
